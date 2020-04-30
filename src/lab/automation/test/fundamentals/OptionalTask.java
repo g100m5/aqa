1b8f0c37de1b90aecb18ced7862d1afd77fea39c
Stanislav Kandyrov <skandyrov@gmail.com>
2020-04-30 15:16:43 +0500

    fix: change description output and use 'foreach' instead 'for'

--- src/lab/automation/test/fundamentals/OptionalTask.java
+++ src/lab/automation/test/fundamentals/OptionalTask.java
@@ -31,23 +31,23 @@ public class OptionalTask {
 
 //    3. Вывести на консоль те числа, длина которых меньше (больше) средней длины по всем числам, а также длину.
 
-    private static String lessThanAverageTaskThree(String[] array) {
+    private static void getLessThanAverageTaskThree(String[] array) {
 
         int average;
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
-        array = sortByStringLengthTaskTwo(array);
+        String [] sortedArray = sortByStringLengthTaskTwo(array);
 
-        average = (array[0].replace("-", "").length()
-                + array[array.length - 1].replace("-", "").length()) / 2;
+        average = (sortedArray[0].replace("-", "").length()
+                + sortedArray[sortedArray.length - 1].replace("-", "").length()) / 2;
 
-        for (int j = 0; j < array.length; j++) {
-            if (array[j].replace("-", "").length() < average) {
-                result += String.format("The number %s is less than average [%d], the length of the number = %s. \n",
-                        array[j], (average), array[j].replace("-", "").length());
+        for (String element : sortedArray) {
+            if (element.replace("-", "").length() < average) {
+                result.append(String.format("The number %s is less than average [%d], the length of the number = %s. \n",
+                        element, average, element.replace("-", "").length()));
             }
         }
-        return result;
+        System.out.println(result);
     }
 
 //    4. Найти число, в котором количество различных цифр минимально. Если таких чисел несколько, найти первое из них.
@@ -57,13 +57,13 @@ public class OptionalTask {
         int number = 0;
         int result = array[0].replace("-", "").length();
 
-        for (int arID = 0; arID < array.length; arID++) {
+        for (String element : array) {
             int unique = 0;
-            for (int i = 0; i < array[arID].replace("-", "").length(); i++) {
+            for (int i = 0; i < element.replace("-", "").length(); i++) {
                 int marker = 1;
-                for (int charId = i; charId < array[arID].replace("-", "").length() - 1; charId++) {
-                    if (array[arID].replace("-", "").charAt(i)
-                            == array[arID].replace("-", "").charAt(charId + 1)) {
+                for (int charId = i; charId < element.replace("-", "").length() - 1; charId++) {
+                    if (element.replace("-", "").charAt(i)
+                            == element.replace("-", "").charAt(charId + 1)) {
                         marker++;
                         break;
                     }
@@ -75,7 +75,7 @@ public class OptionalTask {
             if (unique != 0) {
                 if (unique < result) {
                     result = unique;
-                    number = Integer.parseInt(array[arID]);
+                    number = Integer.parseInt(element);
                 }
             }
         }
@@ -83,14 +83,14 @@ public class OptionalTask {
     }
 
     // print array
-    public static void printArray(String[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+    private static void printArray(String[] array) {
+        for (String str : array) {
+            System.out.print(str + " ");
         }
     }
 
     public static void main(String[] args) {
-//        String[] arr = {"121", "0", "322", "55123555", "551", "899911111", "11111111", "-4444", "2222"};
+//      String[] arr = {"121", "0", "322", "55123555", "551", "899911111", "11111111", "-4444", "2222"};
 
         System.out.println("Hi, please, enter massive length : ");
 
@@ -105,18 +105,17 @@ public class OptionalTask {
 
 
 // #2
-        arr = sortByStringLengthTaskTwo(arr);
-        System.out.println("Numbers entered : ");
-        printArray(arr);
+        String [] sortedArray = sortByStringLengthTaskTwo(arr);
+        System.out.println("Sorting entered numbers: ");
+        printArray(sortedArray);
 // #3
-        System.out.println(lessThanAverageTaskThree(arr));
+        getLessThanAverageTaskThree(arr);
         Collections.reverse(Arrays.asList(arr));
-        System.out.println("Reverse massive : ");
+        System.out.println("Reverse sorted numbers : ");
         printArray(arr);
 // #4
         System.out.println("the number, where the different digits is minimal");
         minUniqueCharsTaskFour(arr);
-
 
     }
 
